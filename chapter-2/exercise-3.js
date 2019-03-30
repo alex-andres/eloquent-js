992d651427944b83586c61006a72387bbd253481
c-andres <charlesalexanderandres@gmail.com>
2019-03-30 12:05:33 -0700

    Add new function to exercise-3.js

--- chapter-2/exercise-3.js
+++ chapter-2/exercise-3.js
@@ -1,22 +1,25 @@
+const printAlternateChar = (string, char1, char2, index) => {
+  if (index % 2 === 0) {
+    string += char1;
+  } else {
+    string += char2;
+  }
+  return string;
+};
+
 const chessBoard = size => {
   let line = '';
   for (let i = 1; i <= size; i++) {
     line = '';
     for (let j = 1; j <= size; j++) {
       if (i % 2 === 0) {
-        if (j % 2 !== 0) {
-          line += '#';
-        } else {
-          line += ' ';
-        }
+        line = printAlternateChar(line, ' ', '#', j);
       } else {
-        if (j % 2 === 0) {
-          line += '#';
-        } else {
-          line += ' ';
-        }
+        line = printAlternateChar(line, '#', ' ', j);
       }
     }
     console.log(line);
   }
 };
+
+chessBoard(20);
